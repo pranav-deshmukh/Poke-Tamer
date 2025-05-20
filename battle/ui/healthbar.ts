c27a1e75e154237e3d975473ba81e5425d4f7c71
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2025-05-20 17:18:55 +0530

    feat: health bar size change function

--- battle/ui/healthbar.ts
+++ battle/ui/healthbar.ts
@@ -5,35 +5,44 @@ export class HealthBar{
     healthBarContainer:Phaser.GameObjects.Container;
     fullWidth:number;
     scaleY:number;
+    leftCap:Phaser.GameObjects.Image;
+    middle:Phaser.GameObjects.Image;
+    rightCap:Phaser.GameObjects.Image;
     constructor(scene:Phaser.Scene, x:number, y:number){
         this.scene = scene;
         this.fullWidth = 360;
         this.scaleY = 0.7;
         this.healthBarContainer = this.scene.add.container(x, y , []);
         this.createHealthBarImages(x, y);
+        this.setMeterPercentage(1);
     }
     get Container(){
         return this.healthBarContainer;
     }
     createHealthBarImages(x: number, y: number) {
           const scaleY = this.scaleY;
-          const leftCap = this.scene.add
+          this.leftCap = this.scene.add
             .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
             .setOrigin(0, 0.5)
             .setScale(1, scaleY);
-          const middle = this.scene.add
-            .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
+          this.middle = this.scene.add
+            .image(this.leftCap.x + this.leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
             .setOrigin(0, 0.5)
             .setScale(1, scaleY);
-          middle.displayWidth = this.fullWidth;
-          const rightCap = this.scene.add
+          this.middle.displayWidth = this.fullWidth;
+          this.rightCap = this.scene.add
             .image(
-              middle.x + middle.displayWidth,
+              this.middle.x + this.middle.displayWidth,
               y,
               HEALTH_BAR_ASSET_KEYS.RIGHT_CAP
             )
             .setOrigin(0, 0.5)
             .setScale(1, scaleY);
-          this.healthBarContainer.add([leftCap, middle, rightCap]);
+          this.healthBarContainer.add([this.leftCap, this.middle, this.rightCap]);
+        }
+        setMeterPercentage(percentage: number=1) {
+            const width = this.fullWidth * percentage;
+            this.middle.displayWidth = width;
+            this.rightCap.x = this.middle.x + this.middle.displayWidth;
         }
 }
